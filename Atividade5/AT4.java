4e4c17805e0c0b93c497fb1020252fa24068b262
BRAIAN <160683261+BRAIAN-UNIPAR@users.noreply.github.com>
2024-04-09 00:59:23 -0300

    AT4.java

--- Atividade5/AT4.java
+++ Atividade5/AT4.java
@@ -1,19 +1,35 @@
-package br.unipar.main;
-
 import java.util.Scanner;
 
-public class ExemploPraticoRepeticao {
+public class ContagemNumeros {
+    private static int countMenorQue100 = 0;
+    private static int countEntre101e200 = 0;
+    private static int countMaiorQue200 = 0;
 
     public static void main(String[] args) {
-
-
-        int[] vetor = new int[5];
+        
         Scanner scanner = new Scanner(System.in);
 
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Informe o seu salario");
-            vetor[i] = scanner.nextInt();
+        System.out.println("Digite 20 numeros:");
 
+        for (int i = 0; i < 4; i++) {
+            int numero = scanner.nextInt();
+            verificarNumero(numero);
+        }
+
+        System.out.println("Quantidade de numeros entre 0 e 100: " + countMenorQue100);
+        System.out.println("Quantidade de numeros entre 101 e 200: " + countEntre101e200);
+        System.out.println("Quantidade de numeros maiores que 200: " + countMaiorQue200);
+
+        scanner.close();
+    }
+
+    public static void verificarNumero(int numero) {
+        if (numero >= 0 && numero <= 100) {
+            countMenorQue100++;
+        } else if (numero <= 200) {
+            countEntre101e200++;
+        } else {
+            countMaiorQue200++;
         }
     }
 }
