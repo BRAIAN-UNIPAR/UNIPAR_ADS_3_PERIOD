9d0b940e8bc1864edeff8e0bb4ee206414fbb805
BRAIAN <160683261+BRAIAN-UNIPAR@users.noreply.github.com>
2024-04-09 00:51:10 -0300

    AT2.java

--- Atividade5/AT2.java
+++ Atividade5/AT2.java
@@ -1,36 +1,29 @@
-package br.unipar.main;
-
 import java.util.Scanner;
 
-public class ExempleWhile {
-
+public class MusicaDosPatinhos {
     public static void main(String[] args) {
-
-        int contador = 0;
-        int opcao = 1;
+        
         Scanner scanner = new Scanner(System.in);
-
-
-//        while (contador <= 10) {
-//            System.out.println("contador " + contador);
-//            contador = contador + 1;
-//
-//        }
-
-
-//        while (true) {
-//            System.out.println("Informe um valor");
-//            opcao = scanner.nextInt();
-//
-//            if (opcao == 0) {
-//                break;
-//            }
-//        }
-
-
-        while (opcao != 0){
-            System.out.println("Informe um valor ");
-            opcao = scanner.nextInt();
+        
+        System.out.print("Digite o numero inicial de patinhos: ");
+        int n = scanner.nextInt();
+        
+        while (n > 0) {
+            System.out.println(n + " patinhos foram passear");
+            System.out.println("Alem das montanhas");
+            System.out.println("Para brincar");
+            System.out.println("A mamae gritou: Qua, qua, qua, qua");
+            n--;
+            if (n > 0) {
+                System.out.println("Mas so " + n + " patinhos voltaram de la");
+            } else {
+               System.out.println("A mamae patinha foi procurar");
+               System.out.println("Alem das montanhas");
+               System.out.println("Na beira do mar");
+               System.out.println("A mamae gritou: Qua, qua, qua, qua");
+               System.out.println("E os patinhos voltaram de la.");
+            }
         }
+        scanner.close();
     }
 }
