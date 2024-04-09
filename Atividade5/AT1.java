36f2f972754e46f951a310cbdbf2656096500efe
BRAIAN <160683261+BRAIAN-UNIPAR@users.noreply.github.com>
2024-04-09 00:44:42 -0300

    AT1.java

--- Atividade5/AT1.java
+++ Atividade5/AT1.java
@@ -1,39 +1,19 @@
-import java.util.Scanner;
-
-public class ExempleFor {
-
+public class FizzBuzz {
+    
     public static void main(String[] args) {
-
-      
-//exemplo
-//        i++ é igual a i = 1 + 1
-//        for(variavel; condicao; incremento){
-//        for (int i = 0; i <= 10;i++) {
-//            System.out.println("i: "+ i);
-//        }
-
-      
-//tabuada
-
-        int num = 0;
-        int ateQuanto = 0;
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Informe o numero da tabuada que deseja");
-        num = scanner.nextInt();
-
-        System.out.println("Até quando desja multiplicar");
-        ateQuanto = scanner.nextInt();
-
-        for (int i = 0; i <=ateQuanto ; i++) {
-            System.out.print(num+ " x "+ i +" = "+ i*num);
-
-            if ((i*num) % 2 == 0){
-                System.out.println(" É par");
-               }else{
-                System.out.println(" É impar");
+        
+        for (int i = 1; i <= 50; i++) {
+            
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
             }
+            
         }
-   }
+    }
 }
