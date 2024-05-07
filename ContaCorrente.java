bdb80b6c2355fffbdf1c4ed88400528fa426ab4c
BRAIAN <160683261+BRAIAN-UNIPAR@users.noreply.github.com>
2024-05-06 22:40:07 -0300

    ContaCorrente.java

---
+++ ContaCorrente.java
@@ -0,0 +1,15 @@
+package br.unipar;
+
+public class ContaCorrente extends HerencaContaBancaria {
+
+    @Override
+    public boolean saque(Double valor) {
+
+        if (saldo >= valor){
+            saldo -= valor;
+            return true;
+        }
+
+        return false;
+    }
+}
